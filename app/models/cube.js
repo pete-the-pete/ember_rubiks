a5b6684e2b9815136f58ab000821e74b9d4590ce
pete_the_pete <pliljegr@gmail.com>
2014-04-30 22:15:36 -0700

    Somthing shows up\! Its just a cube, but its data driven\!

--- app/models/cube.js
+++ app/models/cube.js
@@ -1,4 +1,5 @@
 var Cube = DS.Model.extend({
+  title: DS.attr('string'),
   sections: DS.hasMany('section', {async: true}),
   isSolved: DS.attr('boolean', {defaultValue: false})
 });
@@ -7,34 +8,10 @@ Cube.reopenClass({
   FIXTURES: [
       {
         id: 1,
+        title: 'i am a wild pig',
         isSolved: false,
         sections: [1]
       }
     ]
   });
-
-/*{
-  id: 1,
-    cube:1,
-  cubies: [
-  {
-    id:1,
-    section:1,
-    faces:[
-      {
-        id:1,
-        side: 'left',
-        color: 'orange',
-
-      },
-      {
-        id:2,
-        side: 'back',
-        color: 'green'
-      }
-    ]
-  }
-]
-}*/
-
 export default Cube;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 export default Ember.Route.extend({
   model: function() {
     "use strict";
-    this.store.find('cube');
+    return this.store.find('cube');
   }
 });
