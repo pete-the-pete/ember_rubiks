605becb301df811a5cd95b93a85e2acff3aa3f65
pete_the_pete <pliljegr@gmail.com>
2014-05-17 12:23:38 -0700

    Cube and section tests do stuff!

--- tests/rotations/cube-test.js
+++ tests/rotations/cube-test.js
@@ -2,6 +2,9 @@ import startApp from '../helpers/start-app';
 
 var App;
 
+/* 
+TODO: maybe use this, or something like it, later to programmatically verify the rotations occured
+NOTE: this would need to be ported/included for each type of rotation (cube, layer, section)
 function getComputedAnimationStyles($el) {
   var computed = window.getComputedStyle($el.get(0)),
     key = Array.prototype.reduce.call(computed, function(initial, current, index, array) {
@@ -12,7 +15,7 @@ function getComputedAnimationStyles($el) {
     });
 
   return computed.getPropertyValue();
-}
+}*/
 
 module('Rotations - Cube', {
   setup: function(){
@@ -23,68 +26,40 @@ module('Rotations - Cube', {
   }
 });
 
-function resetCube() {  
-  $('#cube').attr('class',"");
+function resetCube() {
+  return new Promise(function(resolve) {
+    $('#cube').attr('class',"");
+    Ember.run.later(App, resolve, 1000);
+  });
 }
 
 function performVisualConfirmation(rotation, direction) {
-  $("#cube")
-    .addClass(rotation)
-    .addClass(direction)
-    .addClass('step-2');
+  return function() {
+    return new Promise(function(resolve) {
+      $("#cube")
+        .addClass(rotation)
+        .addClass(direction)
+        .addClass('step-2');
+      Ember.run.later(App, resolve, 1000);
+    });
+  };
 }
 
 //All these tests are visual for now, there isn't actually any programmatic validation
 
 asyncTest('cube can rotate', function() {
   expect(0);
-  visit('/').then(function() {
-    new Promise(function(resolve, reject) {
-      performVisualConfirmation('rotateX', 'anticlockwise');
-      Ember.run.later(this, resolve, 1000);
-    }).then(function(resolve, reject) {
-      resetCube();
-      //Ember.run.later(this, resolve, 1000);
-    }).then(function(resolve, reject) {
-      performVisualConfirmation('rotateX', 'clockwise');
-      Ember.run.later(this, resovle, 1000);
-    });
-      /*.then(resetCube)
-      .then(function() {
-        performVisualConfirmation('rotateY', 'anticlockwise');
-      })
-      .then(resetCube)
-      .then(function() {
-        performVisualConfirmation('rotateY', 'clockwise');
-      });*/
-  });
+  visit('/')
+    .then(performVisualConfirmation('rotateX', 'anticlockwise'))
+    .then(resetCube)
+    .then(performVisualConfirmation('rotateX', 'clockwise'))
+    .then(resetCube)
+    .then(performVisualConfirmation('rotateY', 'anticlockwise'))
+    .then(resetCube)
+    .then(performVisualConfirmation('rotateY', 'clockwise'))
+    .then(resetCube)
+    .then(performVisualConfirmation('rotateZ', 'anticlockwise'))
+    .then(resetCube)
+    .then(performVisualConfirmation('rotateZ', 'clockwise'))
+    .then(resetCube)
 });
-
-/*test('cube can rotateY clockwise', function() {
-  expect(0);
-  visit('/').then(function() {
-    performVisualConfirmation('rotateY', 'clockwise');
-  });
-});
-
-test('cube can rotateY anticlockwise', function() {
-  expect(0);
-  visit('/').then(function() {
-    performVisualConfirmation('rotateY', 'anticlockwise');
-  });
-});
-
-test('cube can rotateZ clockwise', function() {
-  expect(0);
-  visit('/').then(function() {
-    performVisualConfirmation('rotateZ', 'clockwise');
-  });
-});
-
-test('cube can rotateZ anticlockwise', function() {
-  expect(0);
-  visit('/').then(function() {
-    performVisualConfirmation('rotateZ', 'anticlockwise');
-  });
-});
-*/
---
+++ tests/rotations/layer-test.js
@@ -0,0 +1,45 @@
+import startApp from '../helpers/start-app';
+
+var App, $layers;
+
+module('Rotations - Layers', {
+  setup: function(){
+    App = startApp();
+  },
+  teardown: function() {
+    App.reset();
+  }
+});
+
+function resetLayer() {
+  return new Promise(function(resolve) {
+    $layers.attr('class','layer');
+    Ember.run.later(App, resolve, 1000);
+  });
+}
+
+function performVisualConfirmation(rotation, direction) {
+  return function() {
+    return new Promise(function(resolve) {
+      $layers
+        .addClass(rotation)
+        .addClass(direction)
+        .addClass('step-2');
+      Ember.run.later(App, resolve, 1000);
+    });
+  };
+}
+
+//All these tests are visual for now, there isn't actually any programmatic validation
+
+asyncTest('layers can rotate', function() {
+  expect(0);
+  visit('/')
+    .then(function() {
+      $layers = $('#cube .layer');
+      performVisualConfirmation('rotateY', 'anticlockwise')()
+      .then(resetLayer)
+      .then(performVisualConfirmation('rotateY', 'clockwise'))
+      .then(resetLayer)
+    });
+});
