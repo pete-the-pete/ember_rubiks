90db95929ee04796da9334658c2093bd0e2dc28f
pete_the_pete <pliljegr@gmail.com>
2014-05-17 18:48:41 -0700

    Front and left faces now rotate

--- tests/rotations/section-test.js
+++ tests/rotations/section-test.js
@@ -1,6 +1,9 @@
 import startApp from '../helpers/start-app';
 
-var App, $sectionCubies;
+var App, 
+  $sectionCubies, 
+  delay = 250,
+  callCount = 0;
 
 module('Rotations - Sections', {
   setup: function(){
@@ -11,47 +14,64 @@ module('Rotations - Sections', {
   }
 });
 
-function resetLayer() {
+function resetCubies() {
   return new Promise(function(resolve) {
     $sectionCubies.attr('class','cubie');
-    Ember.run.later(App, resolve, 2500);
+    Ember.run.later(App, resolve, delay);
   });
 }
 
 function performVisualConfirmation(rotation, direction) {
   return function() {
     return new Promise(function(resolve) {
+      var step = ++callCount % 5;
+      
       $sectionCubies
         .addClass(rotation)
-        .addClass(direction)
-        .addClass('step-1');
-      Ember.run.later(App, resolve, 2500);
+        .addClass(direction);
+
+      if(callCount <= 4) {
+        $sectionCubies.addClass('step-'+step);
+      } else {
+        $sectionCubies.removeClass('step-' + (4 - step));
+      }
+      Ember.run.later(App, resolve, delay);
     });
   };
 }
 
 //All these tests are visual for now, there isn't actually any programmatic validation
 
-asyncTest('front face can rotate', function() {
+asyncTest('faces can rotate', function() {
   expect(0);
   visit('/')
     .then(function() {
       $sectionCubies = $('div.section:last-of-type .cubie');
+      //wind up
       performVisualConfirmation('rotateZ', 'anticlockwise')()
-      .then(resetLayer)
-      .then(performVisualConfirmation('rotateZ', 'clockwise'))
-      .then(resetLayer)
-    });
-});
-
-asyncTest('left face can rotate', function() {
-  expect(0);
-  visit('/')
-    .then(function() {
-      $layers = $('#cube .layer');
-      performVisualConfirmation('rotateZ', 'anticlockwise')()
-      .then(resetLayer)
-      .then(performVisualConfirmation('rotateZ', 'clockwise'))
-      .then(resetLayer)
+      .then(performVisualConfirmation('rotateZ', 'anticlockwise'))
+      .then(performVisualConfirmation('rotateZ', 'anticlockwise'))
+      .then(performVisualConfirmation('rotateZ', 'anticlockwise'))
+      //unwind
+      .then(performVisualConfirmation('rotateZ', 'anticlockwise'))
+      .then(performVisualConfirmation('rotateZ', 'anticlockwise'))
+      .then(performVisualConfirmation('rotateZ', 'anticlockwise'))
+      .then(performVisualConfirmation('rotateZ', 'anticlockwise'))
+      .then(resetCubies)
+      .then(function() {
+        console.debug('it happened');
+        callCount = 0;
+        $sectionCubies = $('div.section div.cubie:first-of-type');
+        //wind up
+        performVisualConfirmation('rotateX', 'anticlockwise')()
+        .then(performVisualConfirmation('rotateX', 'anticlockwise'))
+        .then(performVisualConfirmation('rotateX', 'anticlockwise'))
+        .then(performVisualConfirmation('rotateX', 'anticlockwise'))
+        //unwind
+        .then(performVisualConfirmation('rotateX', 'anticlockwise'))
+        .then(performVisualConfirmation('rotateX', 'anticlockwise'))
+        .then(performVisualConfirmation('rotateX', 'anticlockwise'))
+        .then(performVisualConfirmation('rotateX', 'anticlockwise'));
+      });
     });
 });
