3754b8489b051e8b667ffad79e1776dad32f7b77
Pete <pliljegr@linkedin.com>
2014-05-19 21:34:34 -0700

    Small clean up of model/mock data

--- app/models/cubie.js
+++ app/models/cubie.js
@@ -1,6 +1,4 @@
 var Cubie = DS.Model.extend({
-  row: DS.attr('string'),
-  col: DS.attr('string'),
   section: DS.belongsTo('section'),
   faces: DS.hasMany('face', {async: true})
 });
@@ -18,190 +16,136 @@ Cubie.reopenClass({
     {
       id:1,
       section:1,
-      row: 'row-1',
-      col: 'col-1',
       faces:[LEFT,BACK,TOP]
     },
     {
       id:2,
       section:1,
-      row: 'row-1',
-      col: 'col-2',
       faces:[BACK, TOP]
     },
     {
       id:3,
       section:1,
-      row: 'row-1',
-      col: 'col-3',
       faces:[BACK,RIGHT,TOP]
     },
     {
       id:4,
       section:2,
-      row: 'row-2',
-      col: 'col-1',
       faces:[LEFT,TOP]
     },
     {
       id:5,
       section:2,
-      row: 'row-2',
-      col: 'col-2',
       faces:[TOP]
     },
     {
       id:6,
       section:2,
-      row: 'row-2',
-      col: 'col-3',
       faces:[RIGHT, TOP]
     },
     {
       id:7,
       section:3,
-      row: 'row-3',
-      col: 'col-1',
       faces:[LEFT, FRONT, TOP]
     },
     {
       id:8,
       section:3,
-      row: 'row-3',
-      col: 'col-2',
       faces:[FRONT, TOP]
     },
     {
       id:9,
       section:3,
-      row: 'row-3',
-      col: 'col-3',
       faces:[RIGHT, FRONT, TOP]
     },
     {
       id:10,
       section:4,
-      row: 'row-1',
-      col: 'col-1',
       faces:[LEFT,BACK]
     },
     {
       id:11,
       section:4,
-      row: 'row-1',
-      col: 'col-2',
       faces:[BACK]
     },
     {
       id:12,
       section:4,
-      row: 'row-1',
-      col: 'col-3',
       faces:[BACK,RIGHT]
     },
     {
       id:13,
       section:5,
-      row: 'row-2',
-      col: 'col-1',
       faces:[LEFT]
     },
     {
       id:14,
       section:5,
-      row: 'row-2',
-      col: 'col-2',
       faces:[]
     },
     {
       id:15,
       section:5,
-      row: 'row-2',
-      col: 'col-3',
       faces:[RIGHT]
     },
     {
       id:16,
       section:6,
-      row: 'row-3',
-      col: 'col-1',
       faces:[LEFT, FRONT]
     },
     {
       id:17,
       section:6,
-      row: 'row-3',
-      col: 'col-2',
       faces:[FRONT]
     },
     {
       id:18,
       section:6,
-      row: 'row-3',
-      col: 'col-3',
       faces:[RIGHT, FRONT]
     },
     {
       id:19,
       section:7,
-      row: 'row-1',
-      col: 'col-1',
       faces:[LEFT, BACK, BOTTOM]
     },
     {
       id:20,
       section:7,
-      row: 'row-1',
-      col: 'col-2',
       faces:[BACK, BOTTOM]
     },
     {
       id:21,
       section:7,
-      row: 'row-1',
-      col: 'col-3',
       faces:[BACK, RIGHT, BOTTOM]
     },
     {
       id:22,
       section:8,
-      row: 'row-2',
-      col: 'col-1',
       faces:[LEFT, BOTTOM]
     },
     {
       id:23,
       section:8,
-      row: 'row-2',
-      col: 'col-2',
       faces:[BOTTOM]
     },
     {
       id:24,
       section:8,
-      row: 'row-2',
-      col: 'col-3',
       faces:[RIGHT, BOTTOM]
     },
     {
       id:25,
       section:9,
-      row: 'row-3',
-      col: 'col-1',
       faces:[LEFT, FRONT, BOTTOM]
     },
     {
       id:26,
       section:9,
-      row: 'row-3',
-      col: 'col-2',
       faces:[FRONT, BOTTOM]
     },
     {
       id:27,
       section:9,
-      row: 'row-3',
-      col: 'col-3',
       faces:[RIGHT, FRONT, BOTTOM]
     }
   ]
