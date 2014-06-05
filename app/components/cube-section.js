384644083409593465d01be1bbb1f1e4dbcad03d
Pete <pliljegr@linkedin.com>
2014-06-04 22:14:48 -0700

    Cleaning up components/views/templates

--- app/components/cube-section.js
+++
@@ -1,9 +0,0 @@
-var CubeSection = Ember.Component.extend({
- /* classNameBindings: ['className'],
-  className: function() {
-    "use strict";
-    return 'section-' + this.section.get('id');
-  }.property()*/
-});
-
-export default CubeSection;
