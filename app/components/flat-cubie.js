dc7c2f5c17ad7fb865268a34c429955fe80a339c
pete_the_pete <pliljegr@gmail.com>
2014-08-13 17:59:21 -0700

    Map shows up and updates, but there is some bug with getting the correct cubies

---
+++ app/components/flat-cubie.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import CubieComponent from './cubie';
+
+export default CubieComponent.extend({
+	displayFace: Ember.computed.filter('cubie.faces', function(currFace) {
+		return currFace.id === this.get('face').id;
+	}),
+});
