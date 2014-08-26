04fdd581c9c3dfde3017cfc79112945a37f4f0f6
pete_the_pete <pliljegr@gmail.com>
2014-08-26 09:06:00 -0700

    Created new resources for games and game

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('player', { path: 'players/:player_id' });
+  this.resource('games');
+  this.resource('game', { path: 'games/:game_id' });
 });
 
 export default Router;
