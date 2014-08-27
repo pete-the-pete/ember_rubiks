7ad7d4d7b648eb5433b13723f10342c2bc253236
pete_the_pete <pliljegr@gmail.com>
2014-08-26 19:08:05 -0700

    Showing initial page to create a new game

--- app/router.js
+++ app/router.js
@@ -4,10 +4,22 @@ var Router = Ember.Router.extend({
   location: EmberClientENV.locationType
 });
 
+/**
+* Nested routes so that I can have a header with links to other
+* functionality that is always present.
+*
+* http://emberjs.com/guides/routing/defining-your-routes/
+* http://ugisozols.com/blog/2013/11/05/understanding-nesting-in-emberjs/
+* http://hashrocket.com/blog/posts/ember-routing-the-when-and-why-of-nesting
+*/
 Router.map(function() {
-  this.resource('player', { path: 'players/:player_id' });
-  this.resource('games');
-  this.resource('game', { path: 'games/:game_id' });
+  this.resource('player', { path: 'players/:player_id' }, function() {
+    this.resource('games');
+    this.resource('game', function() {
+      this.route('new')
+    });
+  });
+  
 });
 
 export default Router;
