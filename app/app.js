534c4bda7d728b37ea9f7914b1cb4631adcc9782
pete_the_pete <pliljegr@gmail.com>
2014-09-24 09:57:23 -0700

    Attempting to upgrade ember-cli and unfuck my app/npm/bower install

--- app/app.js
+++ app/app.js
@@ -1,40 +1,16 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-(function() {
-var get = Ember.get, set = Ember.set, EmberHandlebars = Ember.Handlebars;
-
-EmberHandlebars.registerHelper('group', function(options) {
-  var data = options.data,
-      fn   = options.fn,
-      view = data.view,
-      childView;
-
-  childView = view.createChildView(Ember._MetamorphView, {
-    context: get(view, 'context'),
-
-    template: function(context, options) {
-      options.data.insideGroup = true;
-      return fn(context, options);
-    }
-  });
-
-  view.appendChild(childView);
-  console.groupEnd();
-});
-
-})();
-
-
-
 var App = Ember.Application.extend({
-  modulePrefix: 'ember-client', // TODO: loaded via config
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, 'ember-client');
+loadInitializers(App, config.modulePrefix);
 
 export default App;
