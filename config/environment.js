afa29705a2d765f455a89dd815507a6d3d060529
pete_the_pete <pliljegr@gmail.com>
2016-09-01 07:23:36 -0700

    Upgrading

--- config/environment.js
+++ config/environment.js
@@ -20,17 +20,12 @@ module.exports = function(environment) {
     }
   };
 
-  ENV.contentSecurityPolicy = {
-    'connect-src': "'self' wss://s-dal5-nss-16.firebaseio.com",
-    'style-src': "'self' 'unsafe-inline'"
-  }
-
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
